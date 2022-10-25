bc60b1431d3c5c278bfb428f8f986dffc620064f
KassyaMaya <kassya.maya@gmail.com>
2022-10-25 16:33:37 +0300

    added @Before in the hooks class

--- src/test/java/com/ceallo/step_definition/Hooks.java
+++ src/test/java/com/ceallo/step_definition/Hooks.java
@@ -1,12 +1,35 @@
 package com.ceallo.step_definition;
 
+import com.ceallo.pages.DashboardPage;
+import com.ceallo.pages.LoginPage;
+import com.ceallo.utilities.ConfigurationReader;
 import com.ceallo.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+
+    LoginPage loginPage = new LoginPage();
+    DashboardPage dashboardPage = new DashboardPage();
+    @Before
+    public void setupLogin(){
+
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+        loginPage.usernameInputBox.sendKeys(ConfigurationReader.getProperty("username"));
+        loginPage.passwordInputBox.sendKeys(ConfigurationReader.getProperty("password"));
+        loginPage.signInButton.click();
+
+        dashboardPage.talkModuleButton.click();
+
+
+
+    }
 
     @After
     public void teardownScenario(Scenario scenario){
