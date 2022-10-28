6a7ef142363293553d017bfc6f0d66d1b90ab264
KassyaMaya <kassya.maya@gmail.com>
2022-10-28 14:27:03 +0300

    added a list<webElements) in the talk module page, corrected a typo

--- src/test/java/com/ceallo/pages/TalkModulePage.java
+++ src/test/java/com/ceallo/pages/TalkModulePage.java
@@ -2,7 +2,7 @@ package com.ceallo.pages;
 
 import com.ceallo.utilities.Driver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -21,9 +21,6 @@ public class TalkModulePage {
     @FindBy(xpath = "//input[@class='conversation-name']")
     public WebElement nameTheConversation;
 
-    @FindBy(xpath = "//button[@class='navigation__button navigation__button-right primary']")
-    public WebElement addParticipantsBtn;
-
     @FindBy (xpath = "//button[@class='navigation__button navigation__button-right primary']")
     public WebElement createConversationBtn;
 
@@ -33,11 +30,11 @@ public class TalkModulePage {
     @FindBy (xpath = "//span[@class='acli__content__line-one__title']")
     public WebElement newGroupChat;
 
-    @FindBy (xpath = "(//button[@class='icon action-item__menutoggle action-item__menutoggle--default-icon'])[1]")
-    public WebElement conversationSettings;
+    @FindBy (xpath = "//button[@aria-label='Conversation settings']")
+    public List<WebElement> conversationSettings;
 
     @FindBy(xpath = "//span[.='Delete conversation']")
-    public WebElement deleleteConversationOption;
+    public WebElement deleteConversationOption;
 
     @FindBy (xpath = "//button[@aria-label='Participant settings']")
     public List <WebElement> participantsSettings;
